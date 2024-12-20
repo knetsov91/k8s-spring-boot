e6ab379d785c0662789fa37c37ad764982dc5bc8
Konstantin <knetsov91@gmail.com>
2024-12-21 00:21:46 +0200

    Add findByProject_Id in EmployeeRepository.

--- src/main/java/com/ko/k8sspringboot/repository/EmployeeRepository.java
+++ src/main/java/com/ko/k8sspringboot/repository/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package com.ko.k8sspringboot.repository;
 
+import com.ko.k8sspringboot.models.dto.EmployeeDto;
 import com.ko.k8sspringboot.models.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,6 @@ public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> 
 
     @Query("SELECT e FROM EmployeeEntity as e  ORDER BY e.age DESC LIMIT 1")
     Optional<EmployeeEntity> findOldestEmployee();
+
+    List<EmployeeEntity> findByProject_Id(int projectId);
 }
