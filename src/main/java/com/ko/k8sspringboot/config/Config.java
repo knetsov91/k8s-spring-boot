224e914f31a12bc4df96251eab7b8a6363810e77
Konstantin <knetsov91@gmail.com>
2024-12-22 18:53:24 +0200

    Create PasswordEncoder bean.

--- src/main/java/com/ko/k8sspringboot/config/Config.java
+++ src/main/java/com/ko/k8sspringboot/config/Config.java
@@ -3,6 +3,8 @@ package com.ko.k8sspringboot.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Config {
@@ -11,5 +13,10 @@ public class Config {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
