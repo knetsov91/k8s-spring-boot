6e1b81f1ad2d9c208a7c11d4f81daddb323254c4
Konstantin <knetsov91@gmail.com>
2024-12-22 18:52:41 +0200

    Add findByEmail() in UserRepository.

---
+++ src/main/java/com/ko/k8sspringboot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ko.k8sspringboot.repository;
+
+import com.ko.k8sspringboot.models.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
+}
