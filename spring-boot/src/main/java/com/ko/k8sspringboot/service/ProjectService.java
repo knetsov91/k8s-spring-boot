86c8ab5e1b36e3ef39151d9579dada6f01072e62
Konstantin <knetsov91@gmail.com>
2025-03-11 00:36:49 +0200

    Add getProject() in ProjectService.

--- spring-boot/src/main/java/com/ko/k8sspringboot/service/ProjectService.java
+++ spring-boot/src/main/java/com/ko/k8sspringboot/service/ProjectService.java
@@ -12,5 +12,5 @@ import java.util.List;
 public interface ProjectService {
     List<ProjectInfoDto> getProjects();
     ProjectInfoDto create(ProjectCreateDto projectCreateDto);
-
+    ProjectInfoDto getProject(Long id);
 }
