0ab87241b625ccf8c11fdda85770d9d49fd0ab7d
Konstantin <knetsov91@gmail.com>
2025-03-10 23:27:24 +0200

    Add create() in EmployeeService.

--- spring-boot/src/main/java/com/ko/k8sspringboot/service/EmployeeService.java
+++ spring-boot/src/main/java/com/ko/k8sspringboot/service/EmployeeService.java
@@ -1,13 +1,14 @@
 package com.ko.k8sspringboot.service;
 
 import com.ko.k8sspringboot.models.dto.EmployeeDto;
+import com.ko.k8sspringboot.models.entity.EmployeeEntity;
 
 import java.util.List;
 
 public interface EmployeeService {
 
-    public EmployeeDto getOldestEmployee();
-    public List<EmployeeDto> getEmployees();
-
-    public List<EmployeeDto> getAllEmployeesByProject(int projectId);
+     EmployeeDto getOldestEmployee();
+     List<EmployeeDto> getEmployees();
+     EmployeeDto create(EmployeeDto employeeDto);
+     List<EmployeeDto> getAllEmployeesByProject(int projectId);
 }
