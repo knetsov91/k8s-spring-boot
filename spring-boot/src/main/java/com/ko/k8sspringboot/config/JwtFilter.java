38be6e3ea94ded89c2615900bceecb749e4dcfc4
Konstantin <knetsov91@gmail.com>
2025-03-10 18:59:22 +0200

    Fix 403 error for authenticated users by adding call doFilter() in doFilterInternal().

--- spring-boot/src/main/java/com/ko/k8sspringboot/config/JwtFilter.java
+++ spring-boot/src/main/java/com/ko/k8sspringboot/config/JwtFilter.java
@@ -5,8 +5,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,9 +16,9 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -29,7 +31,6 @@ public class JwtFilter extends OncePerRequestFilter {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
     }
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -49,17 +50,22 @@ public class JwtFilter extends OncePerRequestFilter {
             if (userEmail != null && authentication == null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
                 if (jwtService.isTokenValid(jwt, userDetails)) {
+                    SecurityContext context = SecurityContextHolder.getContext();
                     UsernamePasswordAuthenticationToken authtoken = new UsernamePasswordAuthenticationToken(
                             userDetails,
                             null,
                             userDetails.getAuthorities()
                     );
                     authtoken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authtoken);
+                    context.setAuthentication(authtoken);
+
+                    SecurityContextHolder.setContext(context);
                 }
             }
+            filterChain.doFilter(request, response);
 
         } catch (Exception exception) {
+            log.error(exception.getMessage(), exception);
             handlerExceptionResolver.resolveException(request, response, null, exception);
         }
     }
