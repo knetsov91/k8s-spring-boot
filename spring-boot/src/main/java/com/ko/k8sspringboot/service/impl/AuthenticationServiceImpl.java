259ce13c8992b16578472db0bae6630ea984381a
Konstantin <knetsov91@gmail.com>
2025-03-10 23:24:47 +0200

    Create employee for registered user.

--- spring-boot/src/main/java/com/ko/k8sspringboot/service/impl/AuthenticationServiceImpl.java
+++ spring-boot/src/main/java/com/ko/k8sspringboot/service/impl/AuthenticationServiceImpl.java
@@ -2,7 +2,10 @@ package com.ko.k8sspringboot.service.impl;
 
 import com.ko.k8sspringboot.models.dto.LoginUserDto;
 import com.ko.k8sspringboot.models.dto.RegisterUserDto;
+import com.ko.k8sspringboot.models.entity.EmployeeEntity;
 import com.ko.k8sspringboot.models.entity.UserEntity;
+import com.ko.k8sspringboot.models.enums.UserRole;
+import com.ko.k8sspringboot.repository.EmployeeRepository;
 import com.ko.k8sspringboot.repository.UserRepository;
 import com.ko.k8sspringboot.service.AuthenticationService;
 import org.modelmapper.ModelMapper;
@@ -11,18 +14,22 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
+    private final EmployeeRepository employeeRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private ModelMapper modelMapper;
 
-    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, ModelMapper modelMapper) {
+    public AuthenticationServiceImpl(UserRepository userRepository, EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.employeeRepository = employeeRepository;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.modelMapper = modelMapper;
@@ -30,11 +37,24 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public RegisterUserDto register(RegisterUserDto registerUserDto) {
-        UserEntity user = new UserEntity();
-        user.setFullName(registerUserDto.getFullName())
-                .setEmail(registerUserDto.getEmail())
-                .setPassword(passwordEncoder.encode(registerUserDto.getPassword()));
+        UserEntity user = UserEntity.builder()
+                            .email(registerUserDto.getEmail())
+                            .password(passwordEncoder.encode(registerUserDto.getPassword()))
+                            .userRole(UserRole.EMPLOYEE)
+                            .createdAt(LocalDate.now())
+                            .build();
+
         RegisterUserDto map = modelMapper.map(user, RegisterUserDto.class);
+
+        EmployeeEntity employeeEntity = EmployeeEntity.builder()
+                .age(registerUserDto.getAge())
+                .salary(registerUserDto.getSalary())
+                .hireDate(LocalDateTime.now())
+                .firstName(registerUserDto.getFirstName())
+                .lastName(registerUserDto.getLastName())
+                .build();
+        employeeRepository.save(employeeEntity);
+        user.setEmployee(employeeEntity);
         userRepository.save(user);
         return map;
     }
@@ -46,8 +66,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         if (byEmail.isEmpty()) {
             throw new Exception();
         }
-//        LoginUserDto map = modelMapper.map(byEmail, LoginUserDto.class);
-//        return map;
+
         return byEmail.get();
     }
 }
