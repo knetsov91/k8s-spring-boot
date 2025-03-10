e921dde4b06344d51cbfae4cde085db63990e11a
Konstantin <knetsov91@gmail.com>
2025-03-10 14:15:29 +0200

    Change UserEntity primary key to UUID, add Lombok for getters and setters and remove implementing UserDetails

--- spring-boot/src/main/java/com/ko/k8sspringboot/models/entity/UserEntity.java
+++ spring-boot/src/main/java/com/ko/k8sspringboot/models/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.ko.k8sspringboot.models.entity;
 
+import com.ko.k8sspringboot.models.enums.UserRole;
 import jakarta.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,19 +11,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
+@Data
 @Entity
 @Table(name="users")
-public class UserEntity implements UserDetails {
+public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(nullable = false, name="full_name")
     private String fullName;
-
 
     @Column(nullable = false)
     private String email;
@@ -37,87 +39,8 @@ public class UserEntity implements UserDetails {
     @Column(name="udated_at")
     private LocalDate updatedAt;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(nullable = false)
+    private UserRole userRole;
 
-    public UserEntity setId(Long id) {
-        this.id = id;
-        return this;
-    }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public UserEntity setFullName(String fullName) {
-        this.fullName = fullName;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public UserEntity setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
-
-    public UserEntity setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    public UserEntity setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public UserEntity setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-        return this;
-    }
 }
