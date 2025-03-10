ee3a0423b5280c79facdb00e4364ca20c48bb519
Konstantin <knetsov91@gmail.com>
2025-03-11 00:18:54 +0200

    Add create() in ProjectService.

--- spring-boot/src/main/java/com/ko/k8sspringboot/service/ProjectService.java
+++ spring-boot/src/main/java/com/ko/k8sspringboot/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.ko.k8sspringboot.service;
 
 
+import com.ko.k8sspringboot.models.dto.ProjectCreateDto;
 import com.ko.k8sspringboot.models.dto.ProjectInfoDto;
 import com.ko.k8sspringboot.repository.ProjectRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import java.util.List;
 
 public interface ProjectService {
     List<ProjectInfoDto> getProjects();
+    ProjectInfoDto create(ProjectCreateDto projectCreateDto);
+
 }
