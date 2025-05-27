ebaa3237fe9e8014bd82aeb7f10f00d8c27bd457
Konstantin <knetsov91@gmail.com>
2025-05-27 08:22:32 +0300

    Moved AWS CDK directory in the project's root.

---
+++ aws_cdk/bin/aws_cdk.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AwsCdkStack } from '../lib/aws_cdk-stack';
+
+const app = new cdk.App();
+new AwsCdkStack(app, 'AwsCdkStack', {
+
+});
