6c6e05ae94014049262b75da6db416d2fd7bded5
Konstantin <knetsov91@gmail.com>
2025-05-31 12:12:05 +0300

    Add versioning for AuthenticationController's endpoints by change their prefix with /api/v1/auth.

--- src/main/java/com/ko/k8sspringboot/controllers/AuthenticationController.java
+++ src/main/java/com/ko/k8sspringboot/controllers/AuthenticationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
     private JwtService jwtService;
