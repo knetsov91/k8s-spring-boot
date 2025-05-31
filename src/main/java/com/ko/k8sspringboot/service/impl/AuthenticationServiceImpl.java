20af9cab473294a7c28a5e6fa7fd5cbaafe1aef7
Konstantin <knetsov91@gmail.com>
2025-05-31 13:12:03 +0300

    Modify AuthenticationService's login() by change return type to LoginResponse DTO, move JWT logic from controller in it.

--- src/main/java/com/ko/k8sspringboot/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/ko/k8sspringboot/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ko.k8sspringboot.service.impl;
 
+import com.ko.k8sspringboot.models.dto.LoginResponse;
 import com.ko.k8sspringboot.models.dto.LoginUserDto;
 import com.ko.k8sspringboot.models.dto.RegisterUserDto;
 import com.ko.k8sspringboot.models.entity.EmployeeEntity;
@@ -8,15 +9,16 @@ import com.ko.k8sspringboot.models.enums.UserRole;
 import com.ko.k8sspringboot.repository.EmployeeRepository;
 import com.ko.k8sspringboot.repository.UserRepository;
 import com.ko.k8sspringboot.service.AuthenticationService;
+import com.ko.k8sspringboot.service.JwtService;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -25,14 +27,16 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final EmployeeRepository employeeRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
-    private ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
+    private final JwtService jwtService;
 
-    public AuthenticationServiceImpl(UserRepository userRepository, EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, ModelMapper modelMapper) {
+    public AuthenticationServiceImpl(UserRepository userRepository, EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, ModelMapper modelMapper, JwtService jwtService) {
         this.userRepository = userRepository;
         this.employeeRepository = employeeRepository;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.modelMapper = modelMapper;
+        this.jwtService = jwtService;
     }
 
     @Override
@@ -60,13 +64,19 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     @Override
-    public UserEntity login(LoginUserDto registerUserDto) throws Exception {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(registerUserDto.getEmail(), registerUserDto.getPassword()));
-        Optional<UserEntity> byEmail = userRepository.findByEmail(registerUserDto.getEmail());
-        if (byEmail.isEmpty()) {
-            throw new Exception();
-        }
+    public LoginResponse login(LoginUserDto registerUserDto) {
 
-        return byEmail.get();
+        Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        registerUserDto.getEmail(), registerUserDto.getPassword()));
+        UserDetails principal = (UserDetails) authenticate.getPrincipal();
+
+        String token = jwtService.generateToken(principal);
+
+        LoginResponse  loginResponse = new LoginResponse();
+        loginResponse.setToken(token);
+        loginResponse.setExpiresIn(jwtService.getJwtExpiration());
+
+        return loginResponse;
     }
 }
