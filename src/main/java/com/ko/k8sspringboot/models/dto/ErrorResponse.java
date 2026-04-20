3d93ce4ae126f0f198b0a2df6efb9836f269cfb1
User <knetsov91@gmail.com>
2026-04-20 14:27:25 +0300

    fix(api): add missing ErrorResponse DTO used in GlobalExceptionHandler

---
+++ src/main/java/com/ko/k8sspringboot/models/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.ko.k8sspringboot.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
